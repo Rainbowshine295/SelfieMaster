32ee63bdba9557db5cb50565b7b03deee5eb7dfb
Rainbowshine295 <79471824+Rainbowshine295@users.noreply.github.com>
2021-10-07 11:57:42 +0530

    Add files via upload

--- sunglasses.js
+++ sunglasses.js
@@ -1,10 +1,7 @@
-leftEyeX = 0;
-leftEyeY = 0;
-
 function preload() {
 emoji = loadImage("sunglasses.png");
-sunglasses = loadImage("sunglasses 2.png")
 }
+
 function setup() {
     canvas = createCanvas(300,500);
     canvas.center();
@@ -12,31 +9,11 @@ function setup() {
     video.size(300,500);
     video.hide();
 
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
-
 }
-
-function modelLoaded()  {
-    console.log("poseNet initialized! :D");
-}
-
-function gotPoses(results) {
-    if (results.length > 0) {
-        console.log(results);
-        leftEyeX = results[0].pose.leftEye.x;
-        leftEyeY = results[0].pose.leftEye.y;
-        console.log("Left ear X = " + results[0].pose.leftEye.x);
-        console.log("Right ear Y = " + results[0].pose.leftEye.y);
-    }
-}
-
-
 
 function draw() {
     image(video, 0,0,300,500);
-    image(emoji, 120, 300, 175, 175);
-    image(sunglasses, leftEyeX, leftEyeY, 200, 150)
+    image(emoji, 120, 320, 175, 175);
 }
 function take_snapshot() {
     save('Sunglasses Selfie by Rainbowshine.png');
